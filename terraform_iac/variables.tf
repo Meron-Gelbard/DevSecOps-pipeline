c5997b04b108829e2de30c4253bce02571455cca
Meron-Gelbard <merongelbard@gmail.com>
2024-03-22 15:29:00 +0200

    Terraform variables fix

--- terraform_iac/variables.tf
+++ terraform_iac/variables.tf
@@ -1,17 +1,33 @@
-variable "aws_region" {}
-variable "awscli_cred_dict" {}
-variable "app_docker_image" {}
-variable "image_tag" {}
-variable "app_name" {}
-variable "container_port" {}
-variable "app_access_port" {}
-variable "app_instance_type" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "awscli_cred_dict" {
+  default = ["~/.aws/credentials"]
+}
+variable "image_tag" {
+  default = "latest"
+}
+variable "app_name" {
+  default = "WEB-APP"
+}
+variable "container_port" {
+  default = 8080
+}
+variable "app_access_port" {
+  default = 80
+}
+variable "app_instance_type" {
+  default = "t2.micro"
+}
 variable "AZs" {
   type        = list(string)
+  default = ["us-east-1a", "us-east-1b"]
 }
 variable "WEATHER_API_KEY" {}
 variable "LOCATION_API_KEY" {}
 variable "ssl_cert_arn" {}
+variable "app_docker_image" {}
 variable "dockerhub_username" {}
 variable "dockerhub_password" {}
 variable "app_version" {}
+
